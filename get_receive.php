2fedda8f8ca3f74cc0d608f51304218b81cb8e25
ccc-goto <42729329+ccc-goto@users.noreply.github.com>
2021-11-15 10:32:15 +0900

    Add files via upload

---
+++ get_receive.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_GET['name'];
+$address = $_GET['address'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="UTF-8">
+</head>
+<body>
+   <h1>受信ページ</h1>
+   <p>あなたの名前は<?php echo $name;?>さんです。</p>
+   <p>あなたの住所は<?php echo $address;?>です。</p>
+</body>
+</html>
